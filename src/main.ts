3ca6aa7e1b182d17ad73ae0a0830e75eec9ddd42
yvonatron <von@yvonatron.com>
2021-06-23 21:21:48 +0100

    Stream reading/ Fetching

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,27 @@
 import { fromEvent } from 'rxjs';
 import { getComments, getPosts, getUsers } from './index'
 
-let start: number = 0;
-let limit: number = 10;
+class Posts {
 
-// grab DOM elements
-const nextLink = document.querySelector('#next');
-// create an observable of link click
-const nextClick = fromEvent(nextLink, 'click');
-// subscribe to event and fetch next 10 from stream on click
+  start: number;
+  limit: number;
+  filter: Array<any>
 
-const subscription = nextClick.subscribe({
-  // on successful emissions
-  next: () => renderList(postStream, start, limit, ['created_at', 'ASC']),
-  // on errors
-  error: error => console.log(error),
-  // called once on completion
-  complete: () => console.log('complete!')
-});
+  constructor() {
+    this.start = 1;
+    this.limit = 20;
+    this.filter = ['created_at', 'ASC'];
+  }
+}
+
+let nextLink = document.querySelector('#next');// create an observable of link click
+let nextClick = fromEvent(nextLink, 'click');
+
+let postModel = new Posts();
+let start = postModel.start;
+let limit = postModel.limit;
+let filter = postModel.filter;
+let nextStart = start + limit as number;
 
 const postStream = getPosts().then((posts: any) => {
   let data = posts.data;
@@ -25,22 +29,36 @@ const postStream = getPosts().then((posts: any) => {
   renderList(data, start, limit, ['created_at', 'ASC'])
 });
 
-const commentStream = getComments().then((response: any) => {
-  let comments = response.data;
-  comments.every((comment: any) => {
+const commentStream = getComments().then((comments: any) => {
+  let commentsData = comments.data;
+  commentsData.every((comment: any) => {
     let postCommentId: any = comment.postId;
     let postCommentUserId = comment.userId;
     //comments.pipe(filter(ev => postCommentId === postId));
   });
 });
 
-const userStream = getUsers().then((response: any) => {
-  let users = response.data;
-  users.every((user): any => {
+const userStream = getUsers().then((users: any) => {
+  let usersData = users.data;
+  usersData.every((user): any => {
     let userId = user.id;
     // users.pipe(filter(ev => userId === postCommentUserId));
     // users.subscribe(userId => console.log("userId:", userId));
   });
+});
+
+const subscription = nextClick.subscribe({
+  // on successful emissions
+  next: () => {    
+    fetchNext(postStream, nextStart, limit, filter);
+    // console.log('nextStart:' + nextStart);
+    nextStart += limit;
+    // console.log('nextNextStart:' + nextStart);
+  },
+  // on errors
+  error: error => console.log(error),
+  // called once on completion
+  complete: () => console.log('complete!')
 });
 
 //TODO : pagination, detailView, summary Data. e.g. number of posts
@@ -48,24 +66,21 @@ const userStream = getUsers().then((response: any) => {
 
 function renderList(feed: any, start: number, limit: number, filter: string[]) {
   const spinner = document.querySelector('#spinner');
-
-  if (feed && feed.length) {
-    renderItems(feed, start, limit);
-  }
+  
+  renderItems(feed, start, limit);
 
   if (spinner) {
     spinner.remove();
   }
 }
 
-function fetchNext(feed: any, lastOut: number, limit: number) {
-  start = lastOut + 1;
-  renderList(feed, start, limit, ['created_at', 'ASC'])
+function fetchNext(feed: any, start: number, limit: number, filter:any) {
+  renderList(feed, start, limit, filter)
 };
 
-function fetcPrev(feed: any, firstOut: number, limit: number) {
-  start = (firstOut - 10 > 0) ? firstOut : 1;
-  renderList(feed, start, limit, ['created_at', 'ASC'])
+function fetcPrev(feed: any, firstOut: number, limit: number, filter:any) {
+  start = (firstOut - limit > 0) ? firstOut : 1;
+  renderList(feed, start, limit, filter);
 }
 
 function renderItems(feed: any, start: number, limit: number) {
@@ -74,8 +89,6 @@ function renderItems(feed: any, start: number, limit: number) {
   // Clear any prev items from list
   ul.innerHTML = "";
 
-  if (!start) { start = 0 };
-  if (!limit) { limit = 10 };
   let posts = feed.slice(start, limit);
 
   posts.forEach((post: any) => {
