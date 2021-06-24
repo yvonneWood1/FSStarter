4033747cb321712fcf7de0b5d1d487633f47def0
yvonatron <von@yvonatron.com>
2021-06-24 16:59:22 +0100

    Prev and Next paging

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,9 @@ class Posts {
 }
 
 let nextLink = document.querySelector('#next');// create an observable of link click
+let prevLink = document.querySelector('#prev');// create an observable of link click
 let nextClick = fromEvent(nextLink, 'click');
+let prevClick = fromEvent(prevLink, 'click');
 
 let postModel = new Posts();
 let start = postModel.start;
@@ -51,14 +53,41 @@ const userStream = getUsers().then((users: any) => {
   });
 });
 
-const subscription = nextClick.subscribe({
+const nextListener = nextClick.subscribe({
   // on successful emissions
   next: () => {
     fetchNext(feedData, nextStart, limit, filter);
     // console.log('nextStart:' + nextStart);
     nextStart += limit;
-    // console.log('nextNextStart:' + nextStart);
-  },
+    if (nextStart > feedData.length) {
+      // if lastPage of results
+      nextLink.parentElement.classList.remove('active');
+      nextLink.parentElement.classList.add('disabled');
+    }
+    prevLink.parentElement.classList.remove('disabled');
+    prevLink.parentElement.classList.add('active');
+},
+  // on errors
+  error: error => console.log(error),
+  // called once on completion
+  complete: () => console.log('complete!')
+});
+
+const prevListener = prevClick.subscribe({
+  // on successful emissions
+  //Next is Prev in this case.
+  next: () => {
+    nextStart = nextStart - limit;
+    fetchNext(feedData, nextStart, limit, filter);
+    // console.log('nextStart:' + nextStart);
+    if (nextStart < 1) {
+      // if lastPage of results
+      prevLink.parentElement.classList.remove('active');
+      prevLink.parentElement.classList.add('disabled');
+    }
+    nextLink.parentElement.classList.remove('disabled');
+    nextLink.parentElement.classList.add('active');
+},
   // on errors
   error: error => console.log(error),
   // called once on completion
@@ -90,12 +119,12 @@ function fetcPrev(feed: any, firstOut: number, limit: number, filter: any) {
 function renderItems(feed: any, start: number, limit: number) {
 
   const ul = document.querySelector('ul.img-list');
+  // If we are on the last page of results, disable the  Next Link
+
   // Clear any prev items from list
   ul.innerHTML = "";
 
-  let posts = feed;
-
-  posts.forEach((post: any) => {
+  feed.forEach((post: any) => {
     if (post.id >= start && post.id <= start + limit) {
       let li = document.createElement('li');
       let listDiv = document.createElement('div');
